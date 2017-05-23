426f78bf8aa01cb0ded2de341fc6a8b1caf4900f
David Fernandez <i.am.david.fernandez@gmail.com>
2017-05-23 14:30:45 +1000

    <DF> Added wrapper for diff command.

--- functions/svn.fish
+++ functions/svn.fish
@@ -1,3 +1,18 @@
-function svn -d "My package"
-  # Package entry-point
+function svn -d "Subversion command wrapper"
+    # Package entry-point
+
+    set new_args
+
+    for arg in $argv
+        set -e argv[1]
+        switch $arg
+            case 'diff'
+                command svn diff --diff-cmd icdiff --extensions "--line-numbers --no-bold" $argv | less -rFX
+                return
+            case '*'
+                set new_args $new_args $arg
+        end
+    end
+
+    command svn $new_args
 end
